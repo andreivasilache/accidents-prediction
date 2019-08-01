14aacbfbfffc5ba74a7cfedf0997814e24076edb
Vasilache Andrei <andrei.vasilache@assist.ro>
2019-08-01 15:38:39 +0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+  let accidents = loadJSON('json/accidents_uk.json');
+  let weather = loadJSON('json/WeatherUk.json')
+  console.log(accidents);
+  console.log(weather)
+}
+
+function draw() {
+  // put drawing code here
+}
