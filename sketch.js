e685cd02c9edcde50da886a3933e98aeca8723f7
Vasilache Andrei <andrei.vasilache@assist.ro>
2019-08-01 16:11:11 +0300

    added dataSet

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
+
+var dataSet;
+function preload(){
+  dataSet = loadJSON('json/dataSet.json');
+}
+
 function setup() {
-  // put setup code here
-  let accidents = loadJSON('json/accidents_uk.json');
-  let weather = loadJSON('json/WeatherUk.json')
-  console.log(accidents);
-  console.log(weather)
+  console.log(dataSet);
 }
 
 function draw() {
   // put drawing code here
 }
+
