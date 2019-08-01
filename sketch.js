415569cb6dbbb4d5541abd31bdd56adfe20d3ff1
Vasilache Andrei <andrei.vasilache@assist.ro>
2019-08-01 17:24:12 +0300

    added tenserFlow.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
 
 var dataSet;
+var temperatureXS = [];
+var accidentsYS = [];
 function preload(){
+  // 365 dates obtained from every day from 2017
   dataSet = loadJSON('json/dataSet.json');
 }
 
 function setup() {
-  console.log(dataSet);
+  splitData();
+
+  let xs = tf.tensor1d(temperatureXS);
+  let ys = tf.tensor1d(accidentsYS);
+
+  xs.print();
+
 }
 
 function draw() {
   // put drawing code here
+}
+
+function splitData(){
+  let maxTemp = 21;
+  let maxAccidents = 500;
+  let range = maxAccidents / maxTemp;
+  for(let index = 0; index<365 ;index++) {
+    temperatureXS.push((dataSet[index].temperature) / range);
+    accidentsYS.push((dataSet[index].accidents) / range);
+  }
 }
 
